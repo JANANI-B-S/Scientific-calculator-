17245857e505386b437598da0ad59e1b05f221a1
JANANI-B-S <143819251+JANANI-B-S@users.noreply.github.com>
2023-12-26 21:59:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,102 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else {
-      display.innerText += item.id;
-    }
-  };
-});
-const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".dark");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
+const display=document.queryselector("#display");
+
+function designate(a){
+    display.innerText+= a;
+}
+
+
+function del(){
+    var war = display.innerText;
+    display.innerText= war.substring(0, war.length-1);
+}
+
+
+function ac(){
+    display.innerText = "";
+}
+
+
+function equal(){
+   display.innerText= eval(display.innerText);
+}
+
+function sqrt(){
+
+    var qwerty =display.innerText;
+
+    var store = Math.sqrt(qwerty);
+    display.innerText = store;
+}
+
+  
+
+
+
+function rad(){
+    var radian = display.innerText;
+    var radia = (radian*180)/3.14;
+    display.innerText = radia;
+}
+
+function square(){
+    var squared = display.innerText;
+    var redeeemd = Math.pow(squared,2);
+    display.innerText = redeeemd;
+}
+
+function logarithm(){
+    var logarit = display.innerText;
+    var legit = Math.log10(logarit);
+    display.innerText= legit;
+}
+
+function lan(){
+    var logarit = display.innerText;
+    var legit = Math.log(logarit);
+    display.innerText = legit;
+}
+
+function pi(){
+    var pai = display.innerText;
+    var paai = pai * 3.141;
+    display.innerText= paai;
+}
+
+function sine(){
+    var trignometry = display.innerText;
+    var result = Math.sin(trignometry);
+    display.innerText= result;
+
+}
+
+function cos(){
+    var trignometry = display.innerText;
+    var result = Math.cos(trignometry);
+    display.innerText= result;
+    
+}
+function tan(){
+    var trignometry = display.innerText;
+    var result = Math.tan(trignometry);
+    display.innerText= result;
+    
+}
+function asine(){
+    var trignometry = display.innerText;
+    var result = Math.asin(trignometry);
+    display.innerText= result;
+
+}
+
+function acos(){
+    var trignometry = display.innerText;
+    var result = Math.acos(trignometry);
+    display.innerText= result;
+    
+}
+function atan(){
+    var trignometry = display.innerText;
+    var result = Math.atan(trignometry);
+    display.innerText= result;
+    
+}
